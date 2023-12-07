1feb7e14707f170f70b4ee2a61e76b7a4c5e5cfa
陈鑫圣 <cxs21@mails.tsinghua.edu.cn>
2023-12-07 17:05:50 +0800

    refactor MMU to comb logic

--- thinpad_top.srcs/sources_1/new/mmu.sv
+++ thinpad_top.srcs/sources_1/new/mmu.sv
@@ -27,6 +27,10 @@ module mmu (
     // tlb reset
     input wire                          tlb_reset_i,   // for sfence.vma instruction
 
+    // stall & bubble
+    input wire                          stall_i,
+    input wire                          bubble_i,
+
     // wishbone interface
     output reg                          wb_cyc_o,
     output reg                          wb_stb_o,
@@ -38,7 +42,7 @@ module mmu (
     output reg                          wb_we_o,
 
     // data direct pass (for IF1/IF2)
-    output reg                          if1_if2_pc_now,
+    output reg [ADDR_WIDTH-1:0]         if1_if2_pc_now,
 
     // data direct pass (for MEM1/MEM2)
     input wire  [ADDR_WIDTH-1:0]        exe_mem1_pc_now,
@@ -76,14 +80,14 @@ module mmu (
     output reg                          mem1_mem2_mret
 );
 
-reg page_fault;
-reg access_fault;
-assign load_page_fault_o    = page_fault   & read_en_i;
-assign store_page_fault_o   = page_fault   & write_en_i;
-assign instr_page_fault_o   = page_fault   & exe_en_i;
-assign load_access_fault_o  = access_fault & read_en_i;
-assign store_access_fault_o = access_fault & write_en_i;
-assign instr_access_fault_o = access_fault & exe_en_i;
+reg page_fault_o;
+reg access_fault_o;
+assign load_page_fault_o    = page_fault_o   & read_en_i;
+assign store_page_fault_o   = page_fault_o   & write_en_i;
+assign instr_page_fault_o   = page_fault_o   & exe_en_i;
+assign load_access_fault_o  = access_fault_o & read_en_i;
+assign store_access_fault_o = access_fault_o & write_en_i;
+assign instr_access_fault_o = access_fault_o & exe_en_i;
 
 
 wire direct_trans;
@@ -101,7 +105,7 @@ assign pte_addr = (cur_level == 1'b1)
 
 // wishbone interface
 assign wb_stb_o = wb_cyc_o;
-assign wb_adr_o = pte_addr[ADDR_WIDTH-1:0];  // "拼出来的 34 位物理地�?可以直接去掉�?高的两位当作 32 位地�?进行使用�?"
+assign wb_adr_o = pte_addr[ADDR_WIDTH-1:0];  // "拼出来的 34 位物理地址可以直接去掉最高的两位当作 32 位地址进行使用。"
 assign wb_dat_o = {DATA_WIDTH{1'b0}};
 assign wb_sel_o = {{DATA_WIDTH/8}{1'b0}};
 assign wb_we_o  = 1'b0;
@@ -125,107 +129,163 @@ assign tlb_hit = tlb_entry.valid
                 && tlb_entry.tag == vaddr_i[31:31-TLB_TAG_WIDTH+1];
 
 
-always_ff @(posedge clk) begin
+logic [ADDR_WIDTH-1:0] paddr_comb;
+logic                  page_fault_comb;
+logic                  access_fault_comb;
+
+
+always_comb begin: output_ack_and_output_comb
+    // default
+    ack_o             = 1'b0;
+    paddr_comb        = {ADDR_WIDTH{1'b0}};
+    page_fault_comb   = 1'b0;
+    access_fault_comb = 1'b0;
+    // cases
+    casez (state)
+        IDLE: begin
+            if (enable_i) begin
+                // do not translate (i.e., direct translatation)
+                if (direct_trans) begin
+                    if (!paddr_valid(vaddr_i)) begin
+                        raise_access_fault_comb();
+                    end else begin
+                        ack_paddr_comb(vaddr_i);
+                    end
+                // need translation (vaddr -> paddr)
+                end else begin
+                    if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
+                        raise_access_fault_comb();
+                    end else if (tlb_hit) begin
+                        ack_paddr_comb({tlb_entry.ppn[19:0], vaddr_i[11:0]});
+                    end else begin
+                        ack_o = 1'b0;
+                    end
+                end
+            end else begin
+                // default
+            end
+        end
+        FETCH_PTE: begin
+            if (wb_ack_i) begin
+                // If PTE is invalid, raise page fault
+                if (!read_pte.v || (!read_pte.r && read_pte.w)) begin
+                    /* 3. If pte.v = 0, or if pte.r = 0 and pte.w = 1, or if any bits or encodings that are reserved for
+                        future standard use are set within pte, stop and raise a page-fault exception corresponding
+                        to the original access type.*/
+                    raise_page_fault_comb();
+                // Otherwise, the PTE is valid
+                // If it is leaf PTE
+                end else if (read_pte.r || read_pte.x) begin
+                    /* 5. Determine if the requested memory access is allowed by the pte.r, pte.w, pte.x, and pte.u bits, 
+                        given the current privilege mode and the value of the SUM and MXR fields of the mstatus register. 
+                        If not, stop and raise a page-fault exception corresponding to the original access type.*/
+                    // TODO (DONE?): "the value of the SUM and MXR fields of the mstatus register"?
+                    if (   (read_en_i        && !read_pte.r)   
+                        // (!mstatus_mxr_i && read_en_i && !read_pte.r)
+                        // || (mstatus_mxr_i  && read_en_i && !(read_pte.r || read_pte.x))
+                        // /* (Ref: page 23) When MXR=0, only loads from pages marked readable (R=1 in Figure 4.18) will succeed.
+                        //     When MXR=1, loads from pages marked either readable or executable (R=1 or X=1) will succeed. */
+                        // /* mstatus.mxr is not implemented */
+                        || (write_en_i        && !read_pte.w)
+                        || (exe_en_i          && !read_pte.x)
+                        || (mode_i == `MODE_U && !read_pte.u)
+                        || (mode_i == `MODE_S && read_pte.u && !mstatus_sum_i)
+                        /* (Ref: page 23) When SUM=0, S-mode memory accesses to pages that are accessible by U-mode (U=1 in Figure 4.18) will fault. */
+                        ) begin
+                        raise_page_fault_comb();
+                    end else if (cur_level == 1 && read_pte.ppn0 != 0) begin
+                        /* 6. If i > 0 and pte.ppn[i �?? 1 : 0] != 0, this is a misaligned superpage; 
+                            stop and raise a page-fault exception corresponding to the original access type */
+                        raise_page_fault_comb();
+                    end else if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
+                        raise_access_fault_comb();
+                    end else begin
+                        ack_paddr_comb({wb_dat_i[29:10], vaddr_i[11:0]});
+                    end
+                // If it is non-leaf PTE
+                end else begin
+                    /* 4. ... Otherwise, this PTE is a pointer to the next level of the page table. 
+                        Let i = i - 1. If i < 0, stop and raise a page-fault exception corresponding to the original access type. 
+                        Otherwise, let a = pte.ppn × PAGESIZE and go to step 2. */
+                    if (cur_level == 0) begin
+                        raise_page_fault_comb();
+                    end else if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
+                        raise_access_fault_comb();
+                    end else begin
+                        ack_o = 1'b0;
+                    end
+                end
+            end
+        end
+        FETCH_PTE_LV0: begin
+            ack_o = 1'b0;
+        end
+    endcase
+end
+
+
+
+always_ff @(posedge clk) begin: inner_data_and_wishbone
     if (rst) begin
-        // CPU interface
-        ack_o <= 1'b0;
-        paddr_o <= 'b0;
-        page_fault <= 1'b0;
-        access_fault <= 1'b0;
-        // wishbone interface
-        wb_cyc_o <= 1'b0;
-        // inner data
-        state <= FETCH_PTE;
-        cur_level <= 1'b1;
-        lv1_pte <= 'b0;
-        // TLB
-        reset_tlb();
+        reset_state_and_wb();
     end else begin
-        casez (state) 
+        casez (state)
             IDLE: begin
                 if (enable_i) begin
                     // do not translate (i.e., direct translatation)
                     if (direct_trans) begin
-                        if (!paddr_valid(vaddr_i)) begin
-                            raise_access_fault();
-                        end else begin
-                            ack_paddr(vaddr_i);
-                        end
+                        reset_state_and_wb();
                     // need translation (vaddr -> paddr)
                     end else begin
                         if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
-                            raise_access_fault();
+                            reset_state_and_wb();
                         end else if (tlb_hit) begin
-                            ack_paddr_in_tlb();
+                            reset_state_and_wb();
                         end else begin
-                            // CPU interface
-                            ack_o <= 1'b0;
-                            // wishbone interface
-                            wb_cyc_o <= 1'b1;
                             // inner data
-                            state <= FETCH_PTE;
+                            state     <= FETCH_PTE;
                             cur_level <= 1'b1;
+                            lv1_pte   <= 'b0;
+                            // wishbone interface
+                            wb_cyc_o  <= 1'b1;
                         end
                     end
                 end else begin
                     if (tlb_reset_i) begin
                         reset_tlb();
                     end
-                    output_other_data();
+                    reset_state_and_wb();
                 end 
             end
             FETCH_PTE: begin
                 if (wb_ack_i) begin
-                    // If PTE is invalid, raise page fault
                     if (!read_pte.v || (!read_pte.r && read_pte.w)) begin
-                        /* 3. If pte.v = 0, or if pte.r = 0 and pte.w = 1, or if any bits or encodings that are reserved for
-                            future standard use are set within pte, stop and raise a page-fault exception corresponding
-                            to the original access type.*/
-                        raise_page_fault();
-                    // Otherwise, the PTE is valid
-                    // If it is leaf PTE
+                        reset_state_and_wb();
                     end else if (read_pte.r || read_pte.x) begin
-                        /* 5. Determine if the requested memory access is allowed by the pte.r, pte.w, pte.x, and pte.u bits, 
-                            given the current privilege mode and the value of the SUM and MXR fields of the mstatus register. 
-                            If not, stop and raise a page-fault exception corresponding to the original access type.*/
-                        // TODO (DONE?): "the value of the SUM and MXR fields of the mstatus register"?
-                        if (   (read_en_i        && !read_pte.r)   
-                            // mstatus.mxr is not implemented
-                            // (!mstatus_mxr_i && read_en_i && !read_pte.r)
-                            // || (mstatus_mxr_i  && read_en_i && !(read_pte.r || read_pte.x))
-                            // /* (Ref: page 23) When MXR=0, only loads from pages marked readable (R=1 in Figure 4.18) will succeed.
-                            //     When MXR=1, loads from pages marked either readable or executable (R=1 or X=1) will succeed. */
+                        if (   (read_en_i         && !read_pte.r)
                             || (write_en_i        && !read_pte.w)
                             || (exe_en_i          && !read_pte.x)
                             || (mode_i == `MODE_U && !read_pte.u)
                             || (mode_i == `MODE_S && read_pte.u && !mstatus_sum_i)
-                            /* (Ref: page 23) When SUM=0, S-mode memory accesses to pages that are accessible by U-mode (U=1 in Figure 4.18) will fault. */
-                            ) begin
-                            raise_page_fault();
+                        ) begin
+                            reset_state_and_wb();
                         end else if (cur_level == 1 && read_pte.ppn0 != 0) begin
-                            /* 6. If i > 0 and pte.ppn[i �?? 1 : 0] != 0, this is a misaligned superpage; 
-                                stop and raise a page-fault exception corresponding to the original access type */
-                            raise_page_fault();
+                            reset_state_and_wb();
                         end else if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
-                            raise_access_fault();
+                            reset_state_and_wb();
                         end else begin
+                            reset_state_and_wb();
                             // update TLB
                             tlb[tlb_index].tag   <= vaddr_i[31:31-TLB_TAG_WIDTH+1];
                             tlb[tlb_index].ppn   <= wb_dat_i[31:10];
                             tlb[tlb_index].asid  <= satp_i.asid;
                             tlb[tlb_index].valid <= 1'b1;
-                            // ack physical address
-                            ack_paddr({wb_dat_i[29:10], vaddr_i[11:0]});
                         end
-                    // If it is non-leaf PTE
                     end else begin
-                        /* 4. ... Otherwise, this PTE is a pointer to the next level of the page table. 
-                            Let i = i �?? 1. If i < 0, stop and raise a page-fault exception corresponding to the original access type. 
-                            Otherwise, let a = pte.ppn × PAGESIZE and go to step 2. */
                         if (cur_level == 0) begin
-                            raise_page_fault();
+                            reset_state_and_wb();
                         end else if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
-                            raise_access_fault();
+                            reset_state_and_wb();
                         end else begin
                             wb_cyc_o <= 1'b0;   // close wishbone for a cycle
                             lv1_pte <= pte_t'(wb_dat_i);  // cache the just-read level-1 PTE
@@ -242,6 +302,22 @@ always_ff @(posedge clk) begin
         endcase
     end
 end
+
+always_ff @(posedge clk) begin: output_data
+    if (rst) begin
+        output_bubble();
+    end else if (stall_i) begin
+        // do nothing
+    end else if (bubble_i) begin
+        output_bubble();
+    end else begin
+        paddr_o        <= paddr_comb;
+        page_fault_o   <= page_fault_comb;
+        access_fault_o <= access_fault_comb;
+        direct_pass_data();
+    end
+end
+
 
 
 /* ================= utils ================= */
@@ -263,65 +339,31 @@ function automatic logic paddr_valid(
      */
 endfunction
 
-task raise_page_fault();
-    // CPU interface
-    ack_o <= 1'b1;
-    paddr_o <= {ADDR_WIDTH{1'b0}};
-    page_fault <= 1'b1;
-    access_fault <= 1'b0;
-    // wishbone interface
-    wb_cyc_o <= 1'b0;
-    // inner data
-    state <= IDLE;    // TODO: to 'DONE'?
-    cur_level <= 1'b1;
-    output_other_data();
-endtask
+function raise_page_fault_comb();
+    ack_o             = 1'b1;
+    paddr_comb        = {ADDR_WIDTH{1'b0}};
+    page_fault_comb   = 1'b1;
+    access_fault_comb = 1'b0;
+endfunction
 
-task raise_access_fault();
-    // CPU interface
-    ack_o <= 1'b1;
-    paddr_o <= {ADDR_WIDTH{1'b0}};
-    page_fault <= 1'b0;
-    access_fault <= 1'b1;
-    // wishbone interface
-    wb_cyc_o <= 1'b0;
-    // inner data
-    state <= IDLE;    // TODO: to 'DONE'?
-    cur_level <= 1'b1;
-    output_other_data();
-endtask
+function raise_access_fault_comb();
+    ack_o             = 1'b1;
+    paddr_comb        = {ADDR_WIDTH{1'b0}};
+    page_fault_comb   = 1'b0;
+    access_fault_comb = 1'b1;
+endfunction
 
-task automatic ack_paddr(
+function automatic ack_paddr_comb(
     logic [ADDR_WIDTH-1:0] paddr_to_ret
 );
-    // CPU interface
-    ack_o <= 1'b1;
-    paddr_o <= paddr_to_ret;
-    page_fault <= 1'b0;
-    access_fault <= 1'b0;
-    // wishbone interface
-    wb_cyc_o <= 1'b0;
-    // inner data
-    state <= IDLE;
-    cur_level <= 1'b1;
-    output_other_data();
-endtask
+    ack_o             = 1'b1;
+    paddr_comb        = paddr_to_ret;
+    page_fault_comb   = 1'b0;
+    access_fault_comb = 1'b0;
+endfunction
 
-task ack_paddr_in_tlb();
-    // CPU interface
-    ack_o <= 1'b1;
-    paddr_o <= {tlb_entry.ppn[19:0], vaddr_i[11:0]};
-    page_fault <= 1'b0;
-    access_fault <= 1'b0;
-    // wishbone interface
-    wb_cyc_o <= 1'b0;
-    // inner data
-    state <= IDLE;
-    cur_level <= 1'b1;
-    output_other_data();
-endtask
 
-task output_other_data();
+task direct_pass_data();
     // IF1/IF2
     if1_if2_pc_now        <= vaddr_i;
     // MEM1/MEM2
@@ -341,14 +383,43 @@ task output_other_data();
     mem1_mem2_ecall       <= exe_mem1_ecall;
     mem1_mem2_ebreak      <= exe_mem1_ebreak;
     mem1_mem2_mret        <= exe_mem1_mret;
-
 endtask
 
-// function automatic logic[33:0] calc_pte_addr(pte_t prev_pte = 'b0);
-//     return (cur_level == 1'b1) 
-//                 ? (satp_i.ppn << 12)                     + (vaddr_i.vpn1 << 2)
-//                 : ({prev_pte.ppn1, prev_pte.ppn0} << 12) + (vaddr_i.vpn0 << 2);
-// endfunction
+task output_bubble();
+    // CPU interface
+    ack_o             <= 1'b0;
+    paddr_o           <= {ADDR_WIDTH{1'b0}};
+    page_fault_o      <= 1'b0;
+    access_fault_o    <= 1'b0;
+    // IF1/IF2
+    if1_if2_pc_now                  <= 0;
+    // MEM1/MEM2
+    mem1_mem2_pc_now                <= 0;      // only for debug
+    mem1_mem2_mem_re                <= 0;
+    mem1_mem2_rf_wen                <= 0;
+    mem1_mem2_rf_waddr              <= 0;
+    mem1_mem2_rf_wdata              <= 0;  // only for debug
+    mem1_mem2_mem_we                <= 0;
+    mem1_mem2_mem_sel               <= 0;
+    mem1_mem2_mem_wdata             <= 0;
+    mem1_mem2_inst                  <= 0;
+    mem1_mem2_csr_op                <= 0;
+    mem1_mem2_csr_data              <= 0;
+    mem1_mem2_instr_page_fault      <= 0;
+    mem1_mem2_instr_access_fault    <= 0;
+    mem1_mem2_ecall                 <= 0;
+    mem1_mem2_ebreak                <= 0;
+    mem1_mem2_mret                  <= 0;
+endtask
+
+task reset_state_and_wb();
+    // inner data
+    state     <= IDLE;
+    cur_level <= 1'b1;
+    lv1_pte   <= 'b0;
+    // wishbone
+    wb_cyc_o  <= 1'b0;
+endtask
 
 task reset_tlb();
     for (int i = 0; i < N_TLB_ENTRY; ++i) begin
