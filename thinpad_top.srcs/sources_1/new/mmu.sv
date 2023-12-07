3397b00bd01f44ea1c4679d211c5c239044b66f3
陈鑫圣 <cxs21@mails.tsinghua.edu.cn>
2023-12-07 10:50:59 +0800

    cancel mstatus.mxr in MMU

--- thinpad_top.srcs/sources_1/new/mmu.sv
+++ thinpad_top.srcs/sources_1/new/mmu.sv
@@ -8,7 +8,7 @@ module mmu (
     input wire [`MODE_WIDTH-1:0]        mode_i,
     input wire satp_t                   satp_i,
     input wire                          mstatus_sum_i,
-    input wire                          mstatus_mxr_i,
+    // input wire                          mstatus_mxr_i, // mstatus.mxr is not implemented
     input wire vaddr_t                  vaddr_i,  // virtual address
     output reg [ADDR_WIDTH-1:0]         paddr_o,  // physical address
     output reg                          ack_o,    // Only when ack_o == 1, the data that output to CPU interface is valid
@@ -92,7 +92,7 @@ assign pte_addr = (cur_level == 1'b1)
 
 // wishbone interface
 assign wb_stb_o = wb_cyc_o;
-assign wb_adr_o = pte_addr[ADDR_WIDTH-1:0];  // "拼出来的 34 位物理地�?可以直接去掉�?高的两位当作 32 位地�?进行使用�?"
+assign wb_adr_o = pte_addr[ADDR_WIDTH-1:0];  // "拼出来的 34 位物理地�?可以直接去掉�?高的两位当作 32 位地�?进行使用�?"
 assign wb_dat_o = {DATA_WIDTH{1'b0}};
 assign wb_sel_o = {{DATA_WIDTH/8}{1'b0}};
 assign wb_we_o  = 1'b0;
@@ -175,10 +175,12 @@ always_ff @(posedge clk) begin
                             given the current privilege mode and the value of the SUM and MXR fields of the mstatus register. 
                             If not, stop and raise a page-fault exception corresponding to the original access type.*/
                         // TODO (DONE?): "the value of the SUM and MXR fields of the mstatus register"?
-                        if (   (!mstatus_mxr_i && read_en_i && !read_pte.r)
-                            || (mstatus_mxr_i  && read_en_i && !(read_pte.r || read_pte.x))
-                            /* (Ref: page 23) When MXR=0, only loads from pages marked readable (R=1 in Figure 4.18) will succeed.
-                                When MXR=1, loads from pages marked either readable or executable (R=1 or X=1) will succeed. */
+                        if (   (read_en_i        && !read_pte.r)   
+                            // mstatus.mxr is not implemented
+                            // (!mstatus_mxr_i && read_en_i && !read_pte.r)
+                            // || (mstatus_mxr_i  && read_en_i && !(read_pte.r || read_pte.x))
+                            // /* (Ref: page 23) When MXR=0, only loads from pages marked readable (R=1 in Figure 4.18) will succeed.
+                            //     When MXR=1, loads from pages marked either readable or executable (R=1 or X=1) will succeed. */
                             || (write_en_i        && !read_pte.w)
                             || (exe_en_i          && !read_pte.x)
                             || (mode_i == `MODE_U && !read_pte.u)
@@ -187,7 +189,7 @@ always_ff @(posedge clk) begin
                             ) begin
                             raise_page_fault();
                         end else if (cur_level == 1 && read_pte.ppn0 != 0) begin
-                            /* 6. If i > 0 and pte.ppn[i �?? 1 : 0] != 0, this is a misaligned superpage; 
+                            /* 6. If i > 0 and pte.ppn[i �?? 1 : 0] != 0, this is a misaligned superpage; 
                                 stop and raise a page-fault exception corresponding to the original access type */
                             raise_page_fault();
                         end else if (!paddr_valid(pte_addr[ADDR_WIDTH-1:0])) begin
@@ -204,7 +206,7 @@ always_ff @(posedge clk) begin
                     // If it is non-leaf PTE
                     end else begin
                         /* 4. ... Otherwise, this PTE is a pointer to the next level of the page table. 
-                            Let i = i �?? 1. If i < 0, stop and raise a page-fault exception corresponding to the original access type. 
+                            Let i = i �?? 1. If i < 0, stop and raise a page-fault exception corresponding to the original access type. 
                             Otherwise, let a = pte.ppn × PAGESIZE and go to step 2. */
                         if (cur_level == 0) begin
                             raise_page_fault();
@@ -236,9 +238,9 @@ function automatic logic paddr_valid(
         || (paddr == `MTIMECMP_ADDR) || (paddr == `MTIMECMP_ADDR+4)   // CSR - mtimecmp
         || (paddr == `MTIME_ADDR)    || (paddr == `MTIME_ADDR+4)      // CSR - mtime
         || ( ~|((paddr ^ 32'h8000_0000) & 32'hFF80_0000) );           // codes and data [equivalent to (32'h8000_0000 <= paddr && paddr <= 32'h807F_FFFF)]
-    // TODO: 如果增加更多外设，需覝在这里补上相应的物睆地�??区间
+    // TODO: 如果增加更多外设，需覝在这里补上相应的物睆地�??区间
     /* 
-        0x10000000-0x10000007	串坣数杮坊状�??
+        0x10000000-0x10000007	串坣数杮坊状�??
         0x80000000-0x807FFFFF:
             0x80000000-0x800FFFFF	监控程庝代砝
             0x80100000-0x803FFFFF	用户程庝代砝
