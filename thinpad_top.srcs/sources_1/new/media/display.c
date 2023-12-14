4225866568effc02598e0c3ed8c83c441aa63f61
陈鑫圣 <cxs21@mails.tsinghua.edu.cn>
2023-12-14 14:43:01 +0800

    add codes for VGA output

---
+++ thinpad_top.srcs/sources_1/new/media/display.c
@@ -0,0 +1,13 @@
+int main() {
+    char* bram0 = (char *)0x84000000;
+    char* bram1 = (char *)0x85000000;
+    char* flash = (char *)0x83000000;
+    char* selected = (char *)0x86000004;
+    *selected = 1;
+    int i = 0;
+    while(i!=480000) {
+        *(bram0+i) = *(flash+i);
+        i = i+1;
+    }
+    *selected = 0;
+    }
